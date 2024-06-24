16bef71fe8f5bb45449d5fba589bf963a63bbf8b
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-06-24 17:48:54 +0530

    Adding more controllers + Adding HttpSecurity configure in SecurityConfiguration.java for making sure only admins can access all pages

--- src/main/java/org/shiv/configuration/SecurityConfiguration.java
+++ src/main/java/org/shiv/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package org.shiv.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,8 +20,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("foo")
                 .password("foo")
-                .roles("USER");
+                .roles("ADMIN");
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //Use HttpSecurity for method chaining
+        // /** all
+        // .hasRole("USER") for checking only user role
+        //.hasAnyRole("USER","ADMIN") for adding multiple roles
+        http.authorizeRequests()
+                .antMatchers("/**").hasRole("ADMIN")
+                .and().formLogin();
+    }
+
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
--- src/main/java/org/shiv/controller/HomeController.java
+++ src/main/java/org/shiv/controller/HomeController.java
@@ -10,4 +10,12 @@ public class HomeController {
     public String home(){
         return "<h1>Welcome to Home</h1>";
     }
+    @GetMapping("/admin")
+    public String homeAdmin(){
+        return "<h1>Welcome to Home Admin</h1>";
+    }
+    @GetMapping("user/")
+    public String homeUser(){
+        return "<h1>Welcome to Home User</h1>";
+    }
 }
