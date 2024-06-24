37dd193cf37d291af4f708778e4a1cce8d10efb7
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-06-24 13:48:11 +0530

    Default Spring Web Project with HomeController ("/")

---
+++ src/main/java/org/shiv/SpringSecurityApplicationDemoProj.java
@@ -0,0 +1,11 @@
+package org.shiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplicationDemoProj {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplicationDemoProj.class,args);
+    }
+}
---
+++ src/main/java/org/shiv/controller/HomeController.java
@@ -0,0 +1,13 @@
+package org.shiv.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "<h1>Welcome to Home</h1>";
+    }
+}
